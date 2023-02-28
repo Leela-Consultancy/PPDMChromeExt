67977c9eb04fb18771a381acca6a0f5c853def34
Treesa Puravathan Poulose <124056006+TreesaPuravathanPoulose@users.noreply.github.com>
2023-02-28 16:12:25 +0000

    added a function to show last error

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,11 @@
 document.getElementById("fetch-links").addEventListener("click", () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     console.log(tabs);
-    chrome.tabs.sendMessage(tabs[0].id, {message: "fetch_links"});
-    console.log(chrome.tabs.sendMessage(tabs[0].id, {message: "fetch_links"}));
+    if (chrome.runtime.lastError) {
+      onComplete({error: chrome.runtime.lastError});
+    }else{
+      chrome.tabs.sendMessage(tabs[0].id, {message: "fetch_links"});
+      console.log(chrome.tabs.sendMessage(tabs[0].id, {message: "fetch_links"}));
+    }
   });
 });
