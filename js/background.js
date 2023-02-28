ace4603312870413bdeae63c3eada959873b1cfb
Treesa Puravathan Poulose <124056006+TreesaPuravathanPoulose@users.noreply.github.com>
2023-02-28 15:29:37 +0000

    changed to manifest version 3

--- js/background.js
+++ js/background.js
@@ -1,6 +1,16 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
- if (request.action == "report")
-   sendResponse({dom: "The dom that you want to get"});
- else
-   sendResponse({}); // Send nothing..
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "fetch_links") {
+    chrome.scripting.executeScript({
+      target: {tabId: sender.tab.id},
+      func: () => {
+        const links = [];
+        document.querySelectorAll("a").forEach(link => {
+          links.push(link.href);
+        });
+        return links;
+      }
+    }, (results) => {
+      console.log(results[0]);
+    });
+  }
 });
