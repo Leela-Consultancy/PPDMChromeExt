0f51c8179fcfb00f630a5a5161fbfb7262540101
Treesa Puravathan Poulose <124056006+TreesaPuravathanPoulose@users.noreply.github.com>
2023-03-28 15:48:36 +0100

    Removed the unwanted URL from the list

--- js/contentScript.js
+++ js/contentScript.js
@@ -8,12 +8,25 @@ function extractLinks() {
       }
       return null;
     }).filter(link => link !== null);
-    return linkArray;
+    const filteredUrls = linkArray.filter(url => {
+      const pattern1 = /https:\/\/www\.google\.com\/search\?/i;
+      const pattern2 = /https:\/\/support\.google\.com\//i;
+      const pattern3 = /https:\/\/accounts\.google\.com\//i;
+      const pattern4 = /https:\/\/maps\.google\.com\//i;
+      const pattern5 = /https:\/\/myactivity\.google\.com\//i;
+      const pattern6 = /https:\/\/policies\.google\.com\//i;
+      const pattern7 = /https:\/\/www\.google\.com\//i;
+      const pattern8 = /https:\/\/www\.google\.co\./i;
+
+      return !pattern1.test(url) && !pattern2.test(url) && !pattern3.test(url) && !pattern4.test(url) && !pattern5.test(url) && !pattern6.test(url) && !pattern7.test(url) && !pattern8.test(url);
+    });
+    return filteredUrls;
   }
   
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'getLinks') {
       const links = extractLinks();
+      console.log(links);
       sendResponse({ success: true, links });
     }
   });
