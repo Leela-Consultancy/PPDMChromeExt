3b163e8731ddb7919b05cd931e24972d6fd62973
Treesa Puravathan Poulose <124056006+TreesaPuravathanPoulose@users.noreply.github.com>
2023-05-02 15:01:25 +0100

    Extracting cookies,policies and descriptions of each site

--- js/popup.js
+++ js/popup.js
@@ -21,10 +21,16 @@ document.addEventListener('DOMContentLoaded', async () => {
                 const li = document.createElement('li');
                 const a = document.createElement('a');
                 a.href = link;
+                description = fetchData(a.href);
+                console.log(description);
+                // desc = description.htmlInferred['description']
+                // a.textContent = link+"-"+desc;
                 a.textContent = link;
+
                 a.target = '_blank';
                 li.appendChild(a);
                 ul.appendChild(li);
+                // console.log(a.href);
               });
               linksDiv.appendChild(ul);
             } else if (response && !response.success) {
@@ -41,3 +47,32 @@ document.addEventListener('DOMContentLoaded', async () => {
     linksDiv.textContent = 'Error: ' + error.message;
   }
 });
+
+function fetchData(link) {
+
+const url = link;
+
+fetch(url)
+  .then(response => response.text())
+  .then(html => {
+    // Parse the HTML content using a DOM parser
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const descriptionMeta = doc.querySelector("meta[name='description']");
+    const description = descriptionMeta ? descriptionMeta.getAttribute("content") : "";
+    console.log(description);
+    const cookies = document.cookie;
+    console.log(cookies);
+    const securityPolicy = document.securityPolicy;
+    console.log(securityPolicy);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+}
+
+// fetchData();
+
